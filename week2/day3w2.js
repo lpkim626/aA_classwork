4d4f54b7a1bbc27ef13edd68318b313ca0d371c4
Lisa Kim <lpkim09@gmail.com>
2022-10-11 19:03:58 -0400

    update

--- week2/day3w2.js
+++ week2/day3w2.js
@@ -40,3 +40,14 @@ function pairStudents(arr) {
 }
 
 pairStudents(students)
+
+function twoSum(arr, target) {
+    for(let i = 0; i < arr.length; i ++) {
+        for (let j = i + 1 ; j < arr.length; j++) {
+            if(arr[i] + arr[j] === target) {
+                console.log([i,j])
+            }
+        }
+    }
+}
+twoSum([0,1,2,3,4,5,6],5)
